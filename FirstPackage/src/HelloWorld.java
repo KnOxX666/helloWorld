ce7b12e0bd322bacbb3a1f1d4fd20833377ec62e
KnOxX666 <knoxx667@googlemail.com>
2016-11-01 00:09:41 +0100

    #4 added first class for hello world purpose

---
+++ FirstPackage/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by knoxx on 10/31/16.
+ */
+public class HelloWorld {
+    public static void main (String[] args)
+    {
+        // print Hello World!
+        System.out.println("Hello World!");
+    }
+}
